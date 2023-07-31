185975cb408cc7ff479d6795e7d6f3ed629d5e80
Omer <tufanfaruk71@gmail.com>
2023-07-31 13:02:59 +0200

    Initial commit

---
+++ src/test/java/techproed/stepDefinition/AmazonStepDefinition.java
@@ -0,0 +1,27 @@
+package techproed.stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AmazonStepDefinition {
+
+    @Given("kullanici amazon sayfasina gider")
+    public void kullanici_amazon_sayfasina_gider() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("kullanici aramaKutusunda iphone aratir")
+    public void kullanici_arama_kutusunda_iphone_aratir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
